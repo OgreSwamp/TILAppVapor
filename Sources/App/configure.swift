b1146553d6c7db2e244b6e79b29b30adde898e24
Alexey Rashevskiy <alexey@appuchino.ie>
2018-05-01 21:53:31 +0100

    Use PostgreSQL as a database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,4 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 
 /// Called before your application initializes.
@@ -10,7 +10,7 @@ public func configure(
     _ services: inout Services
 ) throws {
     /// Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentPostgreSQLProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -24,24 +24,23 @@ public func configure(
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite: SQLiteDatabase
-    if env.isRelease {
-        /// Create file-based SQLite db using $SQLITE_PATH from process env
-        sqlite = try SQLiteDatabase(storage: .file(path: Environment.get("SQLITE_PATH")!))
-    } else {
-        /// Create an in-memory SQLite database
-        sqlite = try SQLiteDatabase(storage: .memory)
-    }
-
+    
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    
+    let postgresDbConfig = PostgreSQLDatabaseConfig(hostname: hostname, username: username, database: databaseName, password: password)
+    let database = PostgreSQLDatabase(config: postgresDbConfig)
+    databases.add(database: database, as: .psql)
+    
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Acronym.self, database: .sqlite)
+    migrations.add(model: Acronym.self, database: .psql)
     services.register(migrations)
 
 }
